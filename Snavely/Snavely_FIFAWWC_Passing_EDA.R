df0a3cd5e2ba7f19b4714c9942f628aca866ed39
Luke Snavely <lsnavely28@gmail.com>
2025-06-05 14:44:19 -0400

    Updating

--- Snavely/Snavely_FIFAWWC_Passing_EDA.R
+++ Snavely/Snavely_FIFAWWC_Passing_EDA.R
@@ -1,13 +1,18 @@
+##################################################
+# Title: WWC Passing EDA
+# By: Luke and Zach
+##################################################
+
+# Loading in packages/data
 library(tidyverse)
 wwc_passes <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/wwc_passes.csv")
-install.packages("StatsBombR")
+
 
 ## Cleaning the data
-# Replacing NAs with FALSE
+# Replacing NAs with FALSE for pass columns
 wwc_passes_2 <- wwc_passes |>
   select(pass.switch:pass.miscommunication)
 
 wwc_passes_2[is.na(wwc_passes_2)] <- FALSE
 
 wwc_passes[11:23] <- wwc_passes_2[1:13]
-
