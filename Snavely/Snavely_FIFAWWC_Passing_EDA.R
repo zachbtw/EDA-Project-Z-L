286977f6736bf00dcec77865c8849ba6ace43148
Luke Snavely <lsnavely28@gmail.com>
2025-06-05 20:20:21 -0400

    NA filler

--- Snavely/Snavely_FIFAWWC_Passing_EDA.R
+++ Snavely/Snavely_FIFAWWC_Passing_EDA.R
@@ -11,8 +11,11 @@ wwc_passes <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data
 ## Cleaning the data
 # Replacing NAs with FALSE for pass columns
 wwc_passes_2 <- wwc_passes |>
-  select(pass.switch:pass.miscommunication)
+  select(under_pressure, counterpress, pass.switch:pass.miscommunication)
 
 wwc_passes_2[is.na(wwc_passes_2)] <- FALSE
 
-wwc_passes[11:23] <- wwc_passes_2[1:13]
+wwc_passes[c(5, 6, 11:23)] <- wwc_passes_2
+
+table(wwc_passes$pass.type.name)
+table(wwc_passes$pass.outcome.name)
