007c47f1ec784f9475fe7967a245a6a4ace54bb0
Luke Snavely <lsnavely28@gmail.com>
2025-06-06 12:46:01 -0400

    Team stats data frame

--- Snavely/Snavely_FIFAWWC_Passing_EDA.R
+++ Snavely/Snavely_FIFAWWC_Passing_EDA.R
@@ -5,14 +5,17 @@
 
 # Loading in packages/data
 library(tidyverse)
-install.packages("janitor")
+# install.packages("janitor")
+library(janitor)
 wwc_passes <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/wwc_passes.csv")
+
+
+## Cleaning the data
 
 # Cleaning the names
 wwc_passes <- wwc_passes |> 
-  janitor::clean_names()
+  clean_names()
 
-## Cleaning the data
 # Replacing NAs with FALSE for pass columns
 wwc_passes_2 <- wwc_passes |>
   select(under_pressure, counterpress, pass_switch:pass_miscommunication)
@@ -28,6 +31,42 @@ wwc_passes_3 <- wwc_passes |>
 
 wwc_passes[28] <- wwc_passes_3
 
+# Removing "Women's" for the teams
+table(wwc_passes$team_name)
+wwc_passes <- wwc_passes |> 
+  mutate(team_name = str_remove_all(team_name, " Women's"))
+
+# Adding a "knockout_stage" column to differentiate between playoff and non-playoff teams
+wwc_passes <- wwc_passes |>
+  mutate(knockout_stage = ifelse(team_name %in% c("Switzerland", "Spain", 
+                                                  "Netherlands", "South Africa",
+                                                  "Japan", "Norway",
+                                                  "Sweden", "United States",
+                                                  "Australia", "Denmark",
+                                                  "France", "Morocco",
+                                                  "England", "Nigeria",
+                                                  "Colombia", "Jamaica"),
+                                 TRUE, FALSE))
+
+
+## Team statistics df
+team_stats <- wwc_passes |> 
+  group_by(team_name) |> 
+  summarize(pressure_rate = (sum(under_pressure == TRUE)/n() * 100),
+            pass_completion_rate = (sum(pass_outcome_name == "Complete")/n() * 100),
+            switch_rate = (sum(pass_switch == TRUE)/n() * 100),
+            pass_miscommunication_rate = (sum(pass_miscommunication == TRUE)/n() * 100),
+            mean_duration = mean(duration),
+            mean_pass_length = mean(pass_length)) |> 
+  mutate(knockout_stage = ifelse(team_name %in% c("Switzerland", "Spain", 
+                                                  "Netherlands", "South Africa",
+                                                  "Japan", "Norway",
+                                                  "Sweden", "United States",
+                                                  "Australia", "Denmark",
+                                                  "France", "Morocco",
+                                                  "England", "Nigeria",
+                                                  "Colombia", "Jamaica"),
+                                 TRUE, FALSE))
 
 ## EDA
 # Looking at pass outcome due to pressure
@@ -37,7 +76,7 @@ wwc_passes |>
   filter(pass_outcome_name != "Unknown") |> 
   ggplot(aes(y = reorder(pass_outcome_name, Passes), x = Passes, fill = under_pressure)) +
   geom_col() +
-  scale_fill_manual("Under Pressure?", values = c("forestgreen", "gold")) +
+  scale_fill_manual("Under Pressure?", values = c("forestgreen", "goldenrod")) +
   theme_bw() +
   scale_x_sqrt() +
   labs(title = "WWC pass outcomes and their pressure rates",
