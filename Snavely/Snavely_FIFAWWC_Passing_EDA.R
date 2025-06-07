c776d5b00e1aad6e32b92b0d4514b265b74faa89
Luke Snavely <lsnavely28@gmail.com>
2025-06-07 11:28:39 -0400

    Passing summary statistics over the course of a game

--- Snavely/Snavely_FIFAWWC_Passing_EDA.R
+++ Snavely/Snavely_FIFAWWC_Passing_EDA.R
@@ -6,12 +6,16 @@
 # Loading in packages/data
 library(tidyverse)
 # install.packages("janitor")
+# install.packages("ggpubr")
+library(ggpubr)
 library(janitor)
 wwc_passes <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/wwc_passes.csv")
+theme_set(theme_bw())
+
+# Cleaning Data -----------------------------------------------------------
 
 
 ## Cleaning the data
-
 # Cleaning the names
 wwc_passes <- wwc_passes |> 
   clean_names()
@@ -48,7 +52,193 @@ wwc_passes <- wwc_passes |>
                                                   "Colombia", "Jamaica"),
                                  TRUE, FALSE))
 
+# Adding a total time in possession column
+wwc_passes <- wwc_passes |> 
+  mutate(tot_possession = time_in_poss + time_to_poss_end)
 
+
+
+
+# Initial EDA ---------------------------------------------------------------------
+## EDA
+# Looking at pass outcome due to pressure
+wwc_passes |> 
+  group_by(pass_outcome_name, under_pressure) |> 
+  count(pass_outcome_name, name = "Passes") |> 
+  filter(pass_outcome_name != "Unknown") |> 
+  ggplot(aes(y = reorder(pass_outcome_name, Passes), x = Passes, fill = under_pressure)) +
+  geom_col() +
+  scale_fill_manual("Under Pressure?", values = c("forestgreen", "goldenrod")) +
+  theme_bw() +
+  scale_x_sqrt() +
+  labs(title = "WWC pass outcomes and their pressure rates",
+       y = "Pass Outcome")
+
+# Looking at pressure rate and pass completion for teams
+team_stats |> 
+  ggplot(aes(x = pressure_rate, y = pass_completion_rate, color = knockout_stage)) +
+  geom_point(size = 3, alpha = .6) +
+  scale_color_manual("Knockout stage?", values = c("forestgreen", "goldenrod")) +
+  geom_vline(aes(xintercept = mean(pressure_rate)), lty = 2, lwd = 1, color = "red") +
+  geom_hline(aes(yintercept = mean(pass_completion_rate)), lty = 2, lwd = 1, color = "red")
+
+
+# Answering Question 1 ----------------------------------------------------
+# How does a team’s passing ability generally change as the game goes on? 
+wwc_PassingChanges <- wwc_passes |> 
+  filter(period %in% c(1, 2)) |> 
+  mutate(time_range = factor(ifelse(minute < 5, "0 to 5",
+                             ifelse(period == 1 & 5 <= minute & minute < 10, "5 to 10",
+                             ifelse(period == 1 & 10 <= minute & minute < 15, "10 to 15",
+                             ifelse(period == 1 & 15 <= minute & minute < 20, "15 to 20",
+                             ifelse(period == 1 & 20 <= minute & minute < 25, "20 to 25",
+                             ifelse(period == 1 & 25 <= minute & minute < 30, "25 to 30",
+                             ifelse(period == 1 & 30 <= minute & minute < 35, "30 to 35",
+                             ifelse(period == 1 & 35 <= minute & minute < 40, "35 to 40",
+                             ifelse(period == 1 & 40 <= minute & minute < 45, "40 to 45",
+                             ifelse(period == 2 & 45 <= minute & minute < 50, "45 to 50",
+                             ifelse(period == 2 & 50 <= minute & minute < 55, "50 to 55",
+                             ifelse(period == 2 & 55 <= minute & minute < 60, "55 to 60",
+                             ifelse(period == 2 & 60 <= minute & minute < 65, "60 to 65",
+                             ifelse(period == 2 & 65 <= minute & minute < 70, "65 to 70",
+                             ifelse(period == 2 & 70 <= minute & minute < 75, "70 to 75",
+                             ifelse(period == 2 & 75 <= minute & minute < 80, "75 to 80",
+                             ifelse(period == 2 & 80 <= minute & minute < 85, "80 to 85",
+                             ifelse(period == 2 & 85 <= minute & minute < 90, "85 to 90",
+                             ifelse(period == 2 & 90 <= minute, "90+", NA))))))))))))))))))))) |>
+  group_by(time_range, period) |> 
+  summarize(pressure_rate = (sum(under_pressure == TRUE)/n() * 100),
+            pass_completion_rate = (sum(pass_outcome_name == "Complete")/n() * 100),
+            mean_pass_length = mean(pass_length),
+            mean_pass_duration = mean(duration)
+            ) |>
+  na.omit()
+
+# Changing factor levels
+wwc_PassingChanges$time_range <-factor(wwc_PassingChanges$time_range, 
+                                      levels = c("0 to 5", "5 to 10", "10 to 15", "15 to 20", "20 to 25",
+                                      "25 to 30", "30 to 35", "35 to 40", "40 to 45", "45 to 50",
+                                      "50 to 55", "55 to 60", "60 to 65", "65 to 70", "70 to 75",
+                                      "75 to 80", "80 to 85", "85 to 90", "90+"))
+
+# Rearranging columns
+wwc_PassingChanges[1:19, ] <- wwc_PassingChanges[c(1, 10, 2:9, 11:19), ]
+
+# Pivot longer to facet
+wwc_PassingChanges_long <- wwc_PassingChanges |> 
+  select(time_range, pressure_rate, pass_completion_rate) |> 
+  pivot_longer(pressure_rate:pass_completion_rate,
+               names_to = "Stat",
+               values_to = "Val")
+
+# Facet label names
+facet_labels <- c("Pass completion", "Pressure on pass")
+names(facet_labels) <- c("pass_completion_rate", "pressure_rate")
+
+# 2 variable graphing
+wwc_PassingChanges_long |> 
+  ggplot(aes(x = time_range, y = Val, fill = Stat)) +
+  geom_col(alpha = 1) +
+  scale_fill_manual(values = c("#419153", "#d5d4c6")) +
+  facet_wrap(~Stat, nrow = 2, scale = "free_y",
+             labeller = labeller(Stat = facet_labels)) +
+  labs(title = "Average rates throughout 2023 WWC matches",
+       x = "Time of game (minutes)",
+       y = "Rate (%)",
+       caption = "Data courtesy of StatsBomb") +
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = .5, 
+                                  face = "bold",
+                                  size = 20),
+        axis.title = element_text(size = 15,
+                                  face = "bold"),
+        axis.title.x = element_text(vjust = -1),
+        axis.text.x = element_text(angle = 45,
+                                   vjust = .5),
+        axis.text = element_text(size = 10,
+                                 color = "black"),
+        plot.caption = element_text(size = 10,
+                                    face = "italic"),
+        strip.background = element_blank(),
+        strip.text = element_text(size = 15,
+                                  face = "italic",
+                                  color = "grey3"))
+
+
+## Solo Graphs
+# Pass Length
+pass_length_graph <- {
+wwc_PassingChanges |> 
+  ggplot(aes(x = time_range, y = mean_pass_length)) + 
+  geom_col(fill = "#419153") +
+  labs(x = "Time of game (minutes)",
+       y = "Pass length (feet)") +
+  theme(axis.text.x = element_text(angle = 45,
+                             vjust = .5),
+        axis.text = element_text(size = 10,
+                                 color = "black"),
+        axis.title = element_text(size = 15,
+                                  face = "bold"),
+        axis.title.x = element_blank())
+}
+
+pass_duration_graph <- {
+wwc_PassingChanges |> 
+  ggplot(aes(x = time_range, y = mean_pass_duration)) + 
+  geom_col(fill = "#d5d4c6") +
+  labs(x = "Time of game (minutes)",
+       y = "Pass duration (seconds)") +
+  theme(axis.text.x = element_text(angle = 45,
+                                   vjust = .5),
+        axis.text = element_text(size = 10,
+                                 color = "black"),
+        axis.title = element_text(size = 15,
+                                  face = "bold"),
+        axis.title.x = element_blank())
+}
+
+pass_completion_graph <- {  
+wwc_PassingChanges |> 
+  ggplot(aes(x = time_range, y = pass_completion_rate)) + 
+  geom_col(fill = "#4292f6") +
+  labs(x = "Time of game (minutes)",
+       y = "Pass completion rate (%)") +
+  theme(axis.text.x = element_text(angle = 45,
+                                   vjust = .5),
+        axis.text = element_text(size = 10,
+                                 color = "black"),
+        axis.title = element_text(size = 15,
+                                  face = "bold"),
+        axis.title.x = element_blank())
+}
+
+pressure_rate_graph <- {
+wwc_PassingChanges |> 
+  ggplot(aes(x = time_range, y = pressure_rate)) + 
+  geom_col(fill = "#f5c951") +
+  labs(y = "Pressure rate (%)") +
+  theme(axis.text.x = element_text(angle = 45,
+                                   vjust = .5),
+        axis.text = element_text(size = 10,
+                                 color = "black"),
+        axis.title = element_text(size = 15,
+                                  face = "bold"),
+        axis.title.x = element_blank())
+}
+
+title <- "Mean passing statistics over 2023 WWC matches"
+x_axis <- "Time of game (minutes)"
+caption <- "Data courtesy of StatsBomb"
+
+plot <- ggarrange(pass_length_graph, pass_duration_graph, pass_completion_graph, pressure_rate_graph,
+          ncol = 2, nrow = 2)
+annotate_figure(plot,
+                top = text_grob(title, color = "black", face = "bold", size = 20),
+                bottom = text_grob(x_axis, color = "black", face = "bold", size = 15),
+                fig.lab = caption,
+                fig.lab.pos = "bottom.right",
+                fig.lab.face = "italic")
+# Clustering --------------------------------------------------------------
 ## Team statistics df
 team_stats <- wwc_passes |> 
   group_by(team_name) |> 
@@ -68,16 +258,3 @@ team_stats <- wwc_passes |>
                                                   "Colombia", "Jamaica"),
                                  TRUE, FALSE))
 
-## EDA
-# Looking at pass outcome due to pressure
-wwc_passes |> 
-  group_by(pass_outcome_name, under_pressure) |> 
-  count(pass_outcome_name, name = "Passes") |> 
-  filter(pass_outcome_name != "Unknown") |> 
-  ggplot(aes(y = reorder(pass_outcome_name, Passes), x = Passes, fill = under_pressure)) +
-  geom_col() +
-  scale_fill_manual("Under Pressure?", values = c("forestgreen", "goldenrod")) +
-  theme_bw() +
-  scale_x_sqrt() +
-  labs(title = "WWC pass outcomes and their pressure rates",
-       y = "Pass Outcome")
